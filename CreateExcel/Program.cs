b21044c432318f245ba955922e66b67f74a397cc
wubai123@gmail.com <wubai123@gmail.com>
2016-05-06 15:32:43 +0800

    hello
    完成工作

--- CreateExcel/Program.cs
+++ CreateExcel/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 using NPOI.SS.UserModel;
 
 namespace CreateExcel
@@ -15,7 +16,7 @@ namespace CreateExcel
             string Dir = Environment.CurrentDirectory;
             string fileName =  "Test.xlsx";
             string filePath = Path.Combine(Dir,fileName);
-            CreateExcelByNpoi(filePath);
+            CreateExcelByNpoi(filePath,GetTestData());
             Console.WriteLine(string.Format("在目录{0}下创建了文件{1}", Dir, fileName));
             Console.ReadKey();
         }
@@ -24,7 +25,7 @@ namespace CreateExcel
         /// using npoi to create new excel document
         /// </summary>
         /// <param name="filePath"></param>
-        private static void CreateExcelByNpoi(string filePath)
+        private static void CreateExcelByNpoi(string filePath,DataTable dt)
         {
             if (File.Exists(filePath))
             {
@@ -32,20 +33,82 @@ namespace CreateExcel
             }
             IWorkbook workbook = null;
             workbook = new NPOI.XSSF.UserModel.XSSFWorkbook();
-            for (int count = 0; count < 50; count++)
+
+            //创建格式
+            IDataFormat format = workbook.CreateDataFormat();
+
+            ICellStyle HeaderRowStyle = workbook.CreateCellStyle();//第一行
+            ICellStyle DateStyle = workbook.CreateCellStyle();  //日期
+            ICellStyle MoneyStyle = workbook.CreateCellStyle(); //金额
+            ICellStyle PercentStyle = workbook.CreateCellStyle();//百分比
+            //HeaderRowStyle.FillForegroundColor = IndexedColors.Red.Index;
+            HeaderRowStyle.FillBackgroundColor = IndexedColors.Red.Index;
+            HeaderRowStyle.FillPattern = FillPattern.BigSpots;
+            DateStyle.DataFormat = format.GetFormat("yyyy-MM-dd");
+            MoneyStyle.DataFormat = 0x2c;
+            PercentStyle.DataFormat = 9;
+
+
+            ISheet sheet = workbook.CreateSheet("sheetName");
+
+            IRow HeadRow = sheet.CreateRow(0);            
+            
+            for (int i = 0; i < dt.Columns.Count; i++)
             {
-                ISheet sheet = workbook.CreateSheet(string.Format("sheetName{0}",count));
-                for (int i = 0; i < 10; i++)
-                {
-                    IRow row = sheet.CreateRow(i);
-                    for (int j = 0; j < 10; j++)
-                    {
-                        ICell cell = row.CreateCell(j);
-                        cell.SetCellValue(string.Format("{0}:{1}", i, j));
-                    }
-                }
+                ICell cell = HeadRow.CreateCell(i);
+                cell.SetCellValue(dt.Columns[i].ColumnName);
             }
-          
+
+            HeadRow.RowStyle = HeaderRowStyle;
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+
+                IRow row = sheet.CreateRow(i + 1);
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+                    string value = dt.Rows[i][j].ToString();
+                    ICell cell = row.CreateCell(j);
+                    String columnType = dt.Columns[j].DataType.ToString();
+                    switch (columnType)
+                    {
+                        case "System.String":
+                            cell.SetCellValue(value);
+                            break;
+                        case "System.Boolean"://布尔型
+                            bool boolV = false;
+                            bool.TryParse(value, out boolV);
+                            cell.SetCellValue(boolV);
+                            break;
+                        case "System.Int16":
+                        case "System.Int64":
+                        case "System.Byte":
+                        case "System.Int32":
+                            int intV = 0;
+                            int.TryParse(value, out intV);
+                            cell.SetCellValue(intV);
+                            cell.CellStyle = MoneyStyle;
+                            break;
+                        case "System.DateTime":
+                            DateTime t_date;
+                            DateTime.TryParse(value, out t_date);
+                            cell.SetCellValue(t_date);
+                            cell.CellStyle = DateStyle;
+                            break;
+                        case "System.Decimal":
+                        case "System.Double":
+                            Double t_dec;
+                            Double.TryParse(value, out t_dec);
+                            cell.SetCellValue(t_dec);
+                            cell.CellStyle = PercentStyle;
+                            break;
+                        case "System.DBNull"://空值处理
+                            cell.SetCellValue("");
+                            break;
+                        default:
+                            break;
+                    }                            
+                }
+            }         
             FileStream sw = File.Create(filePath);
             workbook.Write(sw);
             sw.Close();
@@ -57,6 +120,37 @@ namespace CreateExcel
         private static void CreateExcelByOpenXmlSDK(string filePath)
         {
             //string filePath="";
+        }
+
+        /// <summary>
+        /// 准备测试数据
+        /// </summary>
+        /// <returns></returns>
+        private static DataTable GetTestData()
+        {
+            DataTable dt = new DataTable();
+            DataColumn dc = null;
+
+            //DataRow dr= dt.NewRow();
+            
+            
+            dc = dt.Columns.Add("name", Type.GetType("System.String"));
+            dc = dt.Columns.Add("Birth", Type.GetType("System.DateTime"));
+            dc = dt.Columns.Add("Count", Type.GetType("System.Int32"));
+            dc = dt.Columns.Add("Money", Type.GetType("System.Decimal"));
+
+            for (int i = 0; i < 100; i++)
+            {
+                DataRow dr = dt.NewRow();
+                dr["name"] = string.Format("姓名_{0}", i);
+                dr["Birth"] = DateTime.Now.AddDays(i);
+                dr["Count"] = i;
+                dr["Money"] = Math.Round((decimal)i / 100, 2);
+                dt.Rows.Add(dr);
+            }
+            return dt;
+
+            
         }
 
 
