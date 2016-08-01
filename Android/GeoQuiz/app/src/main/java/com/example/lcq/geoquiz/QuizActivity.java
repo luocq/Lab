eb4e89f713521cdc260204389e6bf92365a1fecb
luocq <wubai123@gmail.com>
2016-08-01 15:57:30 +0800

    update

--- Android/GeoQuiz/app/src/main/java/com/example/lcq/geoquiz/QuizActivity.java
+++ Android/GeoQuiz/app/src/main/java/com/example/lcq/geoquiz/QuizActivity.java
@@ -4,15 +4,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private Question[] mQuestionBank=new Question[]{
+        new Question(R.string.qustion_africa,true),
+            new Question(R.string.qustion_americas,true),
+            new Question(R.string.qustion_asia,true),
+            new Question(R.string.qustion_mideast,true),
+            new Question(R.string.qustion_oceans,true),
+    };
+    private int mCurrentIndex=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton= (Button)findViewById(R.id.false_button);
