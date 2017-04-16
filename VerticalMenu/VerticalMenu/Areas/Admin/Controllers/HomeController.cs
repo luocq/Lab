0d0ef999097bb30c3b43a56385aa045c7e96034c
luocq <wubai123@gmail.com>
2017-04-16 17:06:12 +0800

    Add New Project

---
+++ VerticalMenu/VerticalMenu/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VerticalMenu.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Menu()
+        {
+            return View();
+        }
+    }
+}
